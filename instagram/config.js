03d21fc27ff456331765988404484d36e2765b7d
stampless <MONICA@STAMPLESS.XYZ>
2016-08-30 20:34:25 -0400

    Update config.js

--- instagram/config.js
+++ instagram/config.js
@@ -1,6 +1,6 @@
 var config = {
   lob_api_key: 'test_791575e4f46ef06abbee9f35014608de899',
-  instagram_redirect_uri: 'stampless.herokuapp.com/',
+  instagram_redirect_uri: 'http://localhost:3000/handleauth',
   instagram_client_id: '90d61f0534e0444e916c173744f21212',
   instagram_client_secret: 'e14034dc39f54ae2a4299f532e008930',
 }
